e62611e80b8dfc64a10900dd1879a42ab8f55f91
mrrazonj <joshrazon132@gmail.com>
2019-09-30 17:50:42 +0800

    added character creation

--- TextRPG.py
+++ TextRPG.py
@@ -1,5 +1,5 @@
 class Entity:
-    def __init__(self, level, desc_race, desc_name, base_hp, base_ap, base_spd, base_atk, base_def,
+    def __init__(self, level, desc_name, desc_race, base_hp, base_ap, base_spd, base_atk, base_def,
                  desc_job, stat_str, stat_dex, stat_con, stat_int, stat_luk):
         self.base_hp = base_hp
         self.base_ap = base_ap
@@ -14,7 +14,7 @@ class Entity:
         self.stat_luk = stat_luk
 
         self.combat_hp = base_hp + (stat_str * 30) + (stat_con * 55)
-        self.combat_ap = base_ap + (stat_dex * 0.03)
+        self.combat_ap = int(base_ap + (stat_dex * 0.03))
         self.combat_spd = base_spd + (stat_dex * 1)
         if self.stat_str * 5 > self.stat_int * 3:
             self.combat_atk = base_atk + (stat_str * 5)
@@ -29,6 +29,7 @@ class Entity:
 
     def show_stats(self):
         print("========================================")
+        print(self.desc_name)
         print("Lv", end='')
         print(self.level, end=' ')
         print(self.desc_race, end=' ')
@@ -41,7 +42,7 @@ class Entity:
         print("Spd: ", end='')
         print(self.combat_spd)
         print("Atk: ", end='')
-        print(self.combat_atk, end='')
+        print(self.combat_atk)
         print("Def: ", end='')
         print(self.combat_def)
         print("========================================")
@@ -59,9 +60,9 @@ class Entity:
 
 
 class Enemy(Entity):
-    def __init__(self, level, desc_race, desc_name, base_hp, base_ap, base_spd, base_atk, base_def,
+    def __init__(self, level, desc_name, desc_race, base_hp, base_ap, base_spd, base_atk, base_def,
                  desc_job, stat_str, stat_dex, stat_con, stat_int, stat_luk, loot1, loot2, loot3, loot4, loot5):
-        super().__init__(level, desc_race, desc_name, base_hp, base_ap, base_spd, base_atk, base_def,
+        super().__init__(level, desc_name, desc_race, base_hp, base_ap, base_spd, base_atk, base_def,
                          desc_job, stat_str, stat_dex, stat_con, stat_int, stat_luk)
 
         self.loot_id = [loot1, loot2, loot3, loot4, loot5]
@@ -71,9 +72,9 @@ class Enemy(Entity):
 
 
 class Player(Entity):
-    def __init__(self, level, desc_race, desc_name, base_hp, base_ap, base_spd, base_atk, base_def,
+    def __init__(self, level, desc_name, desc_race, base_hp, base_ap, base_spd, base_atk, base_def,
                  desc_job, stat_str, stat_dex, stat_con, stat_int, stat_luk):
-        super().__init__(level, desc_race, desc_name, base_hp, base_ap, base_spd, base_atk, base_def,
+        super().__init__(level, desc_name, desc_race, base_hp, base_ap, base_spd, base_atk, base_def,
                          desc_job, stat_str, stat_dex, stat_con, stat_int, stat_luk)
 
         self.experience = 0
@@ -182,13 +183,77 @@ def opening_scene():
     pass
 
 
+def character_creation():
+
+    player_race_dict = {1: "Human",
+                        2: "Elf",
+                        3: "Orc",
+                        4: "Dwarf"
+                        }
+
+    player_job_dict = {1: "Warrior",
+                       2: "Knight",
+                       3: "Ranger",
+                       4: "Mage",
+                       5: "Priest"
+                       }
+
+    is_name_final = False
+    while not is_name_final:
+        desc_name = input("Enter your name> ")
+        print("You will be known as", desc_name, "throughout the realm.")
+
+        name_finalization_input = input("You cannot change your name beyond this point, continue? (y/n) > ")
+        if name_finalization_input == 'y':
+            is_name_final = True
+
+    is_race_final = False
+    while not is_race_final:
+        print("Now, you must choose your race:")
+        for i in player_race_dict:
+            print(i, player_race_dict[i])
+
+        race_selected = int(input("> "))
+        if race_selected > len(player_race_dict) or race_selected < 1:
+            print("You have entered an invalid number, please try again")
+            continue
+        else:
+            desc_race = player_race_dict[race_selected]
+
+        print("You have selected", desc_race, "as your race.")
+        race_finalization_input = input("You cannot change your race beyond this point, continue? (y/n) > ")
+        if race_finalization_input == 'y':
+            is_race_final = True
+
+    is_job_final = False
+    while not is_job_final:
+        print("Finally, you must choose your job:")
+        for i in player_job_dict:
+            print(i, player_job_dict[i])
+
+        job_selected = int(input("> "))
+        if job_selected > len(player_job_dict) or job_selected < 1:
+            print("You have entered an invalid number, please try again")
+            continue
+        else:
+            desc_job = player_job_dict[job_selected]
+
+        print("You have selected", desc_job, "as your race.")
+        job_finalization_input = input("You cannot change your job beyond this point, continue? (y/n) > ")
+        if job_finalization_input == 'y':
+            is_job_final = True
+
+    character_creation_data = [1, desc_name, desc_race, desc_job]
+    return character_creation_data
+
+
 if __name__ == '__main__':
-    dict_race = {"Human": ["Human", "", 125, 3, 8, 6, 5],
-                 "Elf": ["Elf", "", 100, 4, 12, 5, 3],
-                 "Orc": ["Orc", "", 200, 2, 6, 9, 7],
-                 "Troll": ["Troll", "", 250, 1, 2, 15, 10],
-                 "Dwarf": ["Dwarf", "", 150, 3, 7, 8, 8],
-                 "Kobold": ["Kobold", "", 70, 3, 9, 4, 2]
+    dict_race = {"Human": ["Human", 125, 3, 8, 6, 5],
+                 "Elf": ["Elf", 100, 4, 12, 5, 3],
+                 "Orc": ["Orc", 200, 2, 6, 9, 7],
+                 "Troll": ["Troll", 250, 1, 2, 15, 10],
+                 "Dwarf": ["Dwarf", 150, 3, 7, 8, 8],
+                 "Kobold": ["Kobold", 70, 3, 9, 4, 2]
                  }
 
     dict_job = {"Warrior": ["Warrior", 20, 14, 12, 8, 12],
@@ -203,6 +268,12 @@ if __name__ == '__main__':
 
     if main_menu_input == 1:
         opening_scene()
+
+        character_data = character_creation()
+        player = Player(character_data[0], character_data[1], *dict_race[character_data[2]],
+                        *dict_job[character_data[3]])
+        player.show_stats()
+
     elif main_menu_input == 2:
         pass
     else:
