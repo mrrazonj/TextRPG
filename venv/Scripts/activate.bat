763a7b9d9cebf17a8516b21ee10041299d8e51e5
unknown <justin.castillo@valens-research.com>
2019-09-30 09:51:13 +0800

    removed unnecessary files to the repo.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Joshua\PycharmProjects\practice\TextRPG\venv"
+set "VIRTUAL_ENV=C:\Users\Justin\Projects\Pycharm\TextRPG\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
--- venv/Scripts/pip-script.py
+++ venv/Scripts/pip-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\Joshua\PycharmProjects\practice\TextRPG\venv\Scripts\python.exe
+#!C:\Users\Justin\Projects\Pycharm\TextRPG\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
 __requires__ = 'pip==19.0.3'
 import re
